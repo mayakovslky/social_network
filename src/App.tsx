59cfc0fec62777440bf233dfeeb1242a5126942f
mayakovsky <mayakovsky03@gmail.com>
2022-08-29 14:08:16 +0300

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
 
 
 const App = () => {
     return (
         <div className={'app-wrapper'}>
-            <header className={'header'}>
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
-                    alt="Logo"/>
-            </header>
+            <Header/>
             <nav className={'nav'}>
                 <div>
                     <a href={'#'}>Profile</a>
@@ -31,11 +28,11 @@ const App = () => {
                 <div>
                     <img
                         src="https://i0.wp.com/www.luamaralstudio.com/wp-content/uploads/2022/02/soft-aesthetic-desktop-wallpaper-for-computer-by-lu-amaral-studio-6.jpg?resize=980%2C551&ssl=1"
-                        alt="Image"/>
+                        alt="Content"/>
                 </div>
                 <div>
                     <img className={'avatar'} src="https://i.pinimg.com/originals/b4/3b/ba/b43bba3de986fb97865ed342435c4ae5.png"
-                         alt="Image Avatar"/>
+                         alt="Avatar"/>
                     description
                 </div>
                 <div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+                alt="Logo"/>
+        </header>
+        )
+}
+
+export default Header;
