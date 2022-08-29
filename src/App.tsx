6802de1179bb02ef5de0f329a8f1ef96175191e8
mayakovsky <mayakovsky03@gmail.com>
2022-08-29 14:12:07 +0300

    Add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 
 const App = () => {
     return (
         <div className={'app-wrapper'}>
             <Header/>
-            <nav className={'nav'}>
-                <div>
-                    <a href={'#'}>Profile</a>
-                </div>
-                <div>
-                    <a href={'#'}>Messages</a>
-                </div>
-                <div>
-                    <a href={'#'}>News</a>
-                </div>
-                <div>
-                    <a href={'#'}>Music</a>
-                </div>
-                <div>
-                    <a href={'#'}>Settings</a>
-                </div>
-            </nav>
+            <Navbar/>
             <div className={'content'}>
                 <div>
                     <img
